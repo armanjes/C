dc9d92b3a3f8b3122a3b334c8fd71d9edcbe370d
Arman <mdarmanjes@gmail.com>
2025-01-08 14:30:48 +0600

    multidimensional array

--- array/02_example.c
+++ array/02_example.c
@@ -6,12 +6,22 @@ int main() {
     int number[] = {20, 22, 18, 35, 48, 26, 87, 70};
     int smallestNumber = number[0],
         length = sizeof(number) / sizeof(number[0]),
-        i;
+        // i;
+        i = 0;
 
-    for (i = 0; i < length; i++) {
-        if (smallestNumber > number[i]) {
+    // for (i = 0; i < length; i++) {
+    //     if (smallestNumber > number[i]) {
+    //         smallestNumber = number[i];
+    //     }
+    // }
+
+    while (i < length)
+    {
+        if (smallestNumber > number[i])
+        {
             smallestNumber = number[i];
         }
+        i++;
     }
 
     printf("Smallest number: %d", smallestNumber);
--- array/02_multidimensionalArray.c
+++ array/02_multidimensionalArray.c
@@ -10,3 +10,35 @@ A 2D array also known as matrix.
 *** int array[2][3] = {{1, 2}, {1,2,3}}; ***
 The first dimension represents the number of rows [2], while the second dimension represents the number of columns [3].
 */
+
+#include <stdio.h>
+
+int main() {
+    // int number[2][3] = {
+    //     {111, 333, 555},
+    //     {222, 444, 666}
+    // };
+    // printf("%d\n", number[0][2]);
+    // printf("%d\n", number[1][2]);
+
+    int numbers[3][4] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+
+
+    // printf("%d\n", numbers[0][0]);
+    int i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            printf("%d\n", numbers[i][j]);
+        }
+        
+    }
+    
+
+    return 0;
+}
